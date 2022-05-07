abfaea841ce33066dd8d8755439f5c1b6bfb61df
Denys Protsenko <finzigerald953@gmail.com>
2022-05-07 14:22:30 +0300

    Regulator, V1

--- src/main.js
+++ src/main.js
@@ -1,26 +1,39 @@
-document.addEventListener("DOMContentLoaded", function(event){
-    
+document.addEventListener("DOMContentLoaded", function (event) {
+
     const colorItemOne = document.getElementById('color-one');
-    const colorItemTwo  = document.getElementById('color-two');
+    const colorItemTwo = document.getElementById('color-two');
     const colorItemThree = document.getElementById('color-three');
 
-    let eOne = colorItemOne.addEventListener("click", (event) =>{
-        if (!eTwo, !eThree){
-            event.target.style.background = '#911bff'
-        };
+    let defaultColorTwoThree = () => {
+        colorItemTwo.style.background = '#6c757d';
+        colorItemThree.style.background = '#6c757d';
+    }
+
+    let defaultColorOneThree = () => {
+        colorItemOne.style.background = '#6c757d';
+        colorItemThree.style.background = '#6c757d';
+    }
+
+    let defaultColorOneTwo = () => {
+        colorItemTwo.style.background = '#6c757d';
+        colorItemOne.style.background = '#6c757d';
+    }
+
+    let eOne = colorItemOne.addEventListener("click", (event) => {
+        event.target.style.background = '#911bff';
+
+        defaultColorTwoThree();
     });
 
-    let eTwo = colorItemTwo.addEventListener("click", (event) =>{
-        if(!eOne, !eThree){
-            event.target.style.background = '#dfff2a'
-        };
+    let eTwo = colorItemTwo.addEventListener("click", (event) => {
+        event.target.style.background = '#dfff2a';
+        defaultColorOneThree();
+
     });
 
-    let eThree = colorItemThree.addEventListener("click", (event) =>{
-        if(!eOne, !eTwo){
-            event.target.style.background = '#70ff2e'
-        };
+    let eThree = colorItemThree.addEventListener("click", (event) => {
+        event.target.style.background = '#70ff2e';
+        defaultColorOneTwo();
     });
-
 
 })
