9e9440a52c69bf699164dc568470a453955f70d0
Denys Protsenko <finzigerald953@gmail.com>
2022-05-11 17:55:20 +0300

    V3

--- src/main.js
+++ src/main.js
@@ -21,7 +21,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     let eOne = colorItemOne.addEventListener("click", (event) => {
         event.target.style.background = '#911bff';
-
         defaultColorTwoThree();
     });
 
@@ -36,4 +35,79 @@ document.addEventListener("DOMContentLoaded", function (event) {
         defaultColorOneTwo();
     });
 
+
+    /* let content = document.getElementById('content');
+    content = []; */
+
+    const mainButton = document.getElementById('new-task-submit');
+
+    const form = document.querySelector('#new-task-form');
+    const input = document.querySelector('#new-task-input');
+    const list_el = document.querySelector('#tasks')
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+
+        if (!task) {
+            alert('Please add a task')
+        } else {
+
+            const content = document.createElement('div'); //MAIN WRAPPER
+            content.classList.add('task');
+
+            const content_item = document.createElement('div'); //EDITOR-INPUT WRAPPER
+            content_item.classList.add('content');
+            content.appendChild(content_item);
+
+            const indicator = document.createElement('div'); //COLOR INDICATOR
+            indicator.classList.add('task-color-point');
+            content_item.appendChild(indicator);
+
+            const row = document.createElement('input'); //TASK EDITOR-INPUT
+            row.classList.add('task-row');
+            row.value = task;
+            row.type = 'text';
+            row.setAttribute('readonly', 'readonly')
+            content_item.appendChild(row);
+
+            const actions = document.createElement('div'); // BUTTTONS
+            actions.classList.add('actions');
+            content.appendChild(actions);
+
+            const edit = document.createElement('button'); //EDIT-BUTTON
+            edit.classList.add('edit');
+            actions.appendChild(edit);
+            edit.innerHTML = 'edit';
+
+            const del = document.createElement('button'); //DELETE-BUTTON
+            del.classList.add('delete');
+            actions.appendChild(del);
+            del.innerHTML = 'delete';
+
+            list_el.appendChild(content);
+
+            input.value = "";
+
+            edit.addEventListener('click', () => {
+                if (edit.innerHTML.toLowerCase() == 'edit') {
+                    row.removeAttribute('readonly');
+                    row.focus();
+                    edit.innerText = 'save';
+                    console.log(1)
+                } else {
+                    row.setAttribute('readonly', 'readonly');
+                    edit.innerText = 'edit';
+                }
+            })
+
+            del.addEventListener('click', () => {
+                list_el.removeChild(content);
+            })
+
+        }
+    })
 })
+
+
